0a32f83e071f4fdf04f146ba50687ba312bd03a7
YuvaYC <100784397+YuvaYC@users.noreply.github.com>
2025-06-01 17:41:10 +0530

    Marking feature added.
    Now you can mark the added items as done.

--- main.py
+++ main.py
@@ -38,6 +38,22 @@ while True:
     elif info == "l":
         for i in work:
             print("~", i)
-
+            
+    elif info == "m":
+        while True:
+            task = input('Enter the task index number which you need mark done: ')
+            if task == 'e':
+               break
+            try:
+                index = int(task)
+                if 0 <= index < len(work):
+                    work[index] = work[index] + ' - task done!'
+                    new = work[index]
+                    print(new)
+                else:
+                    print("Invalid index.")
+            except ValueError:
+                print("Please enter a valid number.")
+               
     else:
         print("Unknown command. Try: add, remove, l (list), or e (exit).")
